fb28b63b816055bb20fbbe252794cb4cc68c7196
Santiloce <santiloce1@gmail.com>
2025-03-27 12:27:05 +0000

    Entrega CBC

--- ULL/Segundo Cuatrimestre/SeguridadenSI/practica_7/CBC.py
+++ ULL/Segundo Cuatrimestre/SeguridadenSI/practica_7/CBC.py
@@ -34,15 +34,14 @@ def main():
     clave = input("Clave (16 bytes en hexadecimal): ")
     IV = input("IV (16 bytes en hexadecimal): ")
     texto = input("Texto Original (en hexadecimal): ")
+    texto2 = input("Texto segundo: ")
     
     result = CBC(clave, IV, texto)
 
     # Llamar a la función CBC y mostrar el resultado
     print("Bloque de texto cifrado: ", result)
 
-    texto2 = input("Nuevo texto: ")
-    
-    print("Bloque 2", CBC(clave, result, texto2))
+    print("Bloque 2:", CBC(clave, result, texto2))
 
 # Llamar al main para ejecutar el programa
 if __name__ == "__main__":
@@ -54,5 +53,6 @@ Clave: 00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F
 IV: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
 Bloque 1 de Texto Original: 00 11 22 33 44 55 66 77 88 99 AA BB CC DD EE FF
 Texto cifrado: 69 C4 E0 D8 6A 7B 04 30 D8 CD B7 80 70 B4 C5 5A
+Bloque 2 de Texto Cifrado: 4F 63 8C 73 5F 61 43 01 56 78 24 sB1 A2 1A 4F 6A
 """
 
